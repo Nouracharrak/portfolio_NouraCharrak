0da9411055a8812ce19cfd9447ba07eb92605ab0
Noura Charrak <charrak.nora95@gmail.com>
2025-05-03 01:49:59 +0200

    about me translation

--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,14 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 export const aboutMeData = [
 	{
 		id: uuidv4(),
-		bio: 'Bonjour, je m’appelle Noura Charrak et je suis une développeuse web et web mobile passionnée par les nouvelles technologies et l’innovation. Titulaire d’un master en anglais, j’ai eu l’opportunité d’évoluer dans des environnements multiculturels où j’ai développé des compétences en gestion, management d’équipe et organisation. Après plusieurs années d’expérience en management dans la restauration et l’assistance de direction, j’ai choisi de me réorienter vers le développement web, un domaine qui me motive et stimule chaque jour.',
+		bio: 'Hello, my name is Noura Charrak and I am a web and mobile web developer, passionate about new technologies and innovation. I hold a Master’s degree in English and have evolved in multicultural environments where I developed strong skills in management, team leadership, and organization. After several years of experience in restaurant management and executive assistance, I decided to switch to web development—a field that excites and motivates me every day.',
 	},
 	{
 		id: uuidv4(),
-		bio: 'Actuellement en formation de développeur full-stack, j’acquiers des compétences techniques solides en HTML, CSS, JavaScript, PHP (Laravel, Symfony), Node.js, React, SQL/NoSQL, et bien d’autres. Mon parcours m’a également permis de renforcer mon sens de la rigueur, mon adaptabilité et mon esprit d’équipe ; des atouts que je souhaite aujourd’hui mettre au service de projets innovants.',
+		bio: 'Currently, I am undergoing full-stack developer training and gaining solid technical skills in HTML, CSS, JavaScript, PHP (Laravel, Symfony), Node.js, React, SQL/NoSQL, and more. My background has also helped me strengthen my rigor, adaptability, and team spirit—qualities I am eager to bring to innovative projects',
 	},
 	{
 		id: uuidv4(),
-		bio: 'Curieuse, persévérante et à l’aise en anglais comme en français (et arabe en langue maternelle), je cherche une alternance Bac+3/+4 afin de continuer à progresser tout en apportant ma motivation, mon organisation et mon expérience à une équipe dynamique. Passionnée par la programmation, la technologie, les échanges culturels et la gastronomie, j’aime apprendre et relever de nouveaux défis.',
+		bio: 'Curious, persevering, and fluent in both English and French (Arabic is my mother tongue), I am looking for a work-study position (Bac+3/+4 level) to continue growing while contributing my motivation, organization, and experience to a dynamic team. Passionate about programming, technology, cultural exchanges, and gastronomy, I enjoy learning and taking on new challenges.',
 	},
 ];
